137786869497b52f5259cc8cd8e0d789fd7f78a4
coydraheim <36932746+coydraheim@users.noreply.github.com>
2018-04-04 20:53:43 -0500

    Fixing base return
    Fixing base return method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -39,10 +39,8 @@ namespace GradeBook.GradeBooks
                 Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
                 return;
             }
-            if (Students.Count > 5)
-            {
-                base.CalculateStatistics();
-            }            
+            
+            base.CalculateStatistics();                    
         }
 
         public override void CalculateStudentStatistics(string name)
